fded3e4cde32c3b8db16b733bbe85b54e0db5bd4
lenabayeva <lenabayeva@gmail.com>
2016-09-12 18:26:56 +0200

    Improved form validation using ExistingListItemForm that is list aware

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -5,7 +5,9 @@ from django.http import HttpRequest
 from django.template.loader import  render_to_string
 from lists.models import Item, List 
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ( DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+                          ExistingListItemForm, ItemForm,
+                         )
 
 class HomePageTest(TestCase):
 
@@ -125,7 +127,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
@@ -134,18 +136,16 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    from unittest import skip
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_pages(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
         response = self.client.post('/lists/%d/' % (list1.id,),
                 data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
