0e0c713278ac67d234bbb9450ce0074ef1b7ea8a
lenabayeva <lenabayeva@gmail.com>
2016-09-22 15:51:46 +0200

    Custom Persona auth backend + custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+    REQUIRED_FIELDS = ()
+    USERNAME_FIELD = 'email'
+    last_login = models.DateTimeField(default=timezone.now)
+    is_anonymous = False
+    is_authenticated = True
+
+
