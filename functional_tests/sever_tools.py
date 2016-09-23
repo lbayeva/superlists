8a6e827617af4cbe4dc873e6fa7889ee5d20f39d
lenabayeva <lenabayeva@gmail.com>
2016-09-23 17:49:42 +0200

    Try running fab commands in test_my_lists by calling them directly

--- functional_tests/sever_tools.py
+++ functional_tests/sever_tools.py
@@ -1,21 +1,43 @@
-from os import path
-import subprocess
-     
-THIS_FOLDER = path.dirname(path.abspath(__file__))
+#from os import path
+#import subprocess
+#     
+#THIS_FOLDER = path.dirname(path.abspath(__file__))
+#
+#def create_session_on_server(host, email):
+#    return subprocess.check_output(
+#            [
+#                'fab',
+#                'create_session_on_server:email={}'.format(email),
+#                '--host={}'.format(host),
+#                '--hide=everything,status',
+#            ],
+#            cwd=THIS_FOLDER
+#    ).decode().strip()
+#
+#def reset_database(host):
+#    subprocess.check_call(
+#             ['fab', 'reset_database', '--host={}'.format(host)],
+#             cwd=THIS_FOLDER
+#    )
+#
+from fabric.api import env, run
 
-def create_session_on_server(host, email):
-    return subprocess.check_output(
-            [
-                'fab',
-                'create_session_on_server:email={}'.format(email),
-                '--host={}'.format(host),
-                '--hide=everything,status',
-            ],
-            cwd=THIS_FOLDER
-    ).decode().strip()
+def _get_base_folder(host):
+    return '~/sites/' + host
 
-def reset_database(host):
-    subprocess.check_call(
-             ['fab', 'reset_database', '--host={}'.format(host)],
-             cwd=THIS_FOLDER
-    )
+def _get_manage_dot_py(host):
+    return '{path}/virtualenv/bin/python {path}/source/manage.py'.format(
+                path=_get_base_folder(host)
+            )
+
+def reset_database():
+    run('{manage_py} flush --noinput'.format(
+            manage_py=_get_manage_dot_py(env.host)
+    ))
+
+def create_session_on_server(email):
+    session_key = run('{manage_py} create_session {email}'.format(
+        manage_py=_get_manage_dot_py(env.host),
+        email=email,
+    ))
+    print(session_key)
