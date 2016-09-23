e9de0978133797b517f40869b01c105a5df4e18a
lenabayeva <lenabayeva@gmail.com>
2016-09-23 16:23:03 +0200

    Fixed bug: authentication is only valid for a paticular domain

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -3,6 +3,10 @@ var initialize = function(navigator, user, token, urls) {
     $('#id_login').on('click', function() {
         navigator.id.request();
     });
+    $('#id_logout').on('click', function(){
+        navigator.id.logout();
+    });
+
     navigator.id.watch({ loggedInUser: user, 
         onlogin: function(assertion) {
             var deferred = $.post(urls.login,
@@ -11,7 +15,11 @@ var initialize = function(navigator, user, token, urls) {
             deferred.done(function() { window.location.reload(); });
             deferred.fail(function() { navigator.id.logout(); });
         },
-        onlogout: function() {}
+        onlogout: function() {i
+            var deferred = $.post(urls.logout, {csrfmiddlewaretoken: token} )
+            deferred.done(function() { window.location.reload(); });
+            deferred.fail(function() { navigator.id.logout(); });
+        }
     });
 };
 
