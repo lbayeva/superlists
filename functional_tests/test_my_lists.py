b6675ceb63e18853b5f9c1220f636d145decce37
lenabayeva <lenabayeva@gmail.com>
2016-09-24 16:50:01 +0200

    Add logging and a left out test

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -16,6 +16,17 @@ class MyListTest(FunctionalTest):
         self.browser.get(self.server_url + "/404_no_such_url/")
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
-            value=session.session_key,
+            value=session_key,
             path='/',
          ))
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = 'edith@example.com'
+        self.browser.get(self.server_url)
+        self.wait_to_be_logged_out(email)
+
+        # Edith is a logged in user
+        self.create_pre_authenticated_session(email)
+        self.browser.get(self.server_url)
+
+        self.wait_to_be_logged_in(email)
